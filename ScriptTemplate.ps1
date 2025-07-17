ae54515aaf443d7f05e9cf923a501e467ff611b2
mzierer78 <76684304+mzierer78@users.noreply.github.com>
2025-07-17 12:09:12 +0200

    Update

--- ScriptTemplate.ps1
+++ ScriptTemplate.ps1
@@ -26,6 +26,8 @@ param(
 
 #region loading modules, scripts & files
 $here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$ScriptNameTmp = ($MyInvocation.MyCommand.Name).Split(".")
+$ScriptName = $ScriptNameTmp[0]
 #
 # load configuration XML(s)
 $XMLPath = Join-Path $here -ChildPath $XMLName
@@ -34,14 +36,17 @@ $XMLPath = Join-Path $here -ChildPath $XMLName
 # we write one logfile and append each script execution
 [string]$global:Logfile = $ConfigFile.Configuration.Logfile.Name
 If ($Logfile -eq "Default"){
-    $global:Logfile = Join-Path $here -ChildPath "ScriptTemplate.log"
+    $global:Logfile = Join-Path "C:\Windows\Logs" -ChildPath ($ScriptName + ".log")
+} else {
+  $global:Logfile = Join-Path $ConfigFile.Configuration.Logfile.Path -ChildPath $LogFile
 }
 $lfTmp = $global:Logfile.Split(".")
 $global:Logfile = $lfTmp[0] + (Get-Date -Format yyyyMMdd) + "." + $lfTmp[1]
 #
-# Debug Mode
+<# Debug Mode
 # If the parameter '-Debug' is specified or debug in XMLfile is set to "true", script activates debug mode
-# when debug mode is active, debug messages will be dispalyed in console windows
+# when debug mode is active, debug messages will be dispalyed in console windows 
+#>
 #
 If ($ConfigFile.Configuration.debug -eq "true"){
     $Debug = $true
@@ -55,22 +60,156 @@ If ($Debug){
 
 #region functions
 function  Write-Log {
-    param
-    (
-      [Parameter(Mandatory=$true)]
-      $Message
-    )
-    If($Debug){
-      Write-Debug -Message $Message
-    }
-  
-    $msgToWrite =  ('{0} :: {1}' -f (Get-Date -Format yyy-MM-dd_HH-mm-ss),$Message)
-  
-    if($global:Logfile)
-    {
-      $msgToWrite | out-file -FilePath $global:Logfile -Append -Encoding utf8
-    }
+  param
+  (
+    [Parameter(Mandatory=$true)]
+    $Message
+  )
+  If($Debug){
+    Write-Debug -Message $Message
   }
+
+  $msgToWrite =  ('{0} :: {1}' -f (Get-Date -Format yyy-MM-dd_HH-mm-ss),$Message)
+
+  if($global:Logfile)
+  {
+    $msgToWrite | out-file -FilePath $global:Logfile -Append -Encoding utf8
+  }
+}
+function Exit-Script {
+  param (
+    $ExitCode = 0
+  )
+  Write-Log -Message "start function Exit-Script"
+  Write-Log -Message "Exit Script execution with Error Code $ExitCode"
+  Exit $ExitCode
+}
+function Show-ActionBox {
+  param(
+    $Button1Text = "Cancel",
+    $Button2Text = "Back",
+    $Button3Text = "Next",
+    $TextToDisplay = "No Text provided"
+  )
+  <# TextToDisplay
+  Damit der Text angezeigt werden kann, muss er in einem bestimmten Format
+  vorliegen. Nachfolgend ein Beispiel
+
+  $TextToDisplay = @"
+    Es existiert bereits ein Benutzer Home Verzeichnis: 
+
+    $UserHomePath
+
+    Dieses Verzeichnis muss entfernt / archiviert werden
+    bevor der Benutzer angelegt werden kann
+
+    Benutzeranlage wird daher abgebrochen
+"@
+
+  Nur wichtig sind hierbei die beiden @ zeichen. Alles was zwischen Ihnen
+  steht, wird als Text angezeigt. Es können auch Variablen verwendet werden.
+  #>
+  
+  Write-Log -Message "start function Show-ActionBox"
+  Add-Type -AssemblyName System.Windows.Forms
+  # Erstelle ein Formular
+  $form = New-Object Windows.Forms.Form
+  $form.Text = $FormText
+  $form.Size = New-Object Drawing.Size($FormSize)
+
+  # Erstelle ein Label für den Text
+  $label = New-Object Windows.Forms.Label
+  $label.Text = $TextToDisplay
+  $label.Location = New-Object Drawing.Point(20, 20)
+  $label.AutoSize = $true
+
+  # Erstelle die Schaltfläche "Cancel"
+  $cancelButton = New-Object Windows.Forms.Button
+  $cancelButton.Text = $Button1Text
+  $cancelButton.Location = New-Object Drawing.Point(20, 270)
+  $cancelButton.DialogResult = [Windows.Forms.DialogResult]::Cancel
+
+  # Erstelle die Schaltfläche "Back"
+  $backButton = New-Object Windows.Forms.Button
+  $backButton.Text = $Button2Text
+  $backButton.Location = New-Object Drawing.Point(120, 270)
+  $backButton.DialogResult = [Windows.Forms.DialogResult]::Retry
+
+  # Erstelle die Schaltfläche "Next"
+  $continueButton = New-Object Windows.Forms.Button
+  $continueButton.Text = $Button3Text
+  $continueButton.Location = New-Object Drawing.Point(220, 270)
+  $continueButton.DialogResult = [Windows.Forms.DialogResult]::OK
+
+  # Füge die Steuerelemente zum Formular hinzu
+  $form.Controls.Add($label)
+  $form.Controls.Add($cancelButton)
+  $form.Controls.Add($backButton)
+  $form.Controls.Add($continueButton)
+
+  # Zeige das Formular als Dialog an
+  $result = $form.ShowDialog()
+
+  # Überprüfe das Ergebnis
+  if ($result -eq [Windows.Forms.DialogResult]::OK) {
+      Write-Log -Message "User clicked $Button3Text"
+  } 
+  if ($result -eq [Windows.Forms.DialogResult]::Retry){
+      Write-Log -Message "User clicked $Button2Text"
+      $Global:Back = $true
+  }
+  if ($result -eq [Windows.Forms.DialogResult]::Cancel) {
+      Write-Log -Message "User clicked $Button1Text"
+      $Global:ExitScript = $true
+  }
+
+  # Gib das Formular frei
+  $form.Dispose()
+  write-log -Message "end function Show-ActionBox"
+  
+}
+function Show-InfoBox {
+  param (
+    $TextToDisplay
+  )
+  Write-Log -Message "start function Show-InfoBox"
+  # Anzeigen der Infos, welche zum Erstellen des Benutzers verwendet werden
+  
+  Write-Log -Message "Show message box"
+  Add-Type -AssemblyName System.Windows.Forms
+  # Erstelle ein Formular
+  $form = New-Object Windows.Forms.Form
+  $form.Text = $FormText
+  $form.Size = New-Object Drawing.Size($FormSize)
+
+  # Erstelle ein Label für den Text
+  $label = New-Object Windows.Forms.Label
+  $label.Text = $TextToDisplay
+  $label.Location = New-Object Drawing.Point(20, 20)
+  $label.AutoSize = $true
+
+  # Erstelle die Schaltfläche "OK"
+  $okButton = New-Object Windows.Forms.Button
+  $okButton.Text = "OK"
+  $okButton.Location = New-Object Drawing.Point(130, 270)
+  $okButton.DialogResult = [Windows.Forms.DialogResult]::OK
+
+  # Füge die Steuerelemente zum Formular hinzu
+  $form.Controls.Add($label)
+  $form.Controls.Add($okButton)
+  
+  # Zeige das Formular als Dialog an
+  $result = $form.ShowDialog()
+  
+  # Überprüfe das Ergebnis
+  if ($result -eq [Windows.Forms.DialogResult]::OK) {
+      Write-Log -Message "User clicked OK"
+  } 
+
+  # Gib das Formular frei
+#  $form.Dispose()
+  write-log -Message "end function Show-ADInfos"
+}
 #endregion
 
 #region write basic infos to log
